45bfeae606de5f100812e9c7412cd2048802dec0
saad810 <saadameen810@gmail.com>
2024-09-04 00:00:22 +0500

    axios base url update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3500";
-// export const BASE_URL = "https://server-trading-app-production.up.railway.app";
+// export const BASE_URL = "http://localhost:3500";
+export const BASE_URL = "https://server-trading-app-production.up.railway.app";
 
 export default axios.create({
   baseURL: BASE_URL,
